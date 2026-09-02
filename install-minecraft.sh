4fb26452f3b325f53bfa37cddb13fe15ab4ec981
NBWgamer2012 <nwaterman2012@gmail.com>
2026-09-02 15:22:39 -0700

    the files
    yea

---
+++ install-minecraft.sh
@@ -0,0 +1,19 @@
+apt update
+apt upgrade -y
+pkg install wget openssl-tool proot tar -y
+hash -r
+wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Alpine/alpine.sh
+bash alpine.sh
+./start-alpine.sh
+apk update
+apk upgrade
+apk add ca-certificates
+update-ca-certificates
+apk add nano
+apk add openjdk17-jre -y
+java -version
+mkdir -p minecraft
+cd minecraft
+wget https://drive.google.com/drive/folders/1MYwwIGcewyJ_U-Tt66HbtGMWZ5WPgRJa?usp=sharing
+cd minecraftserver
+java -Xmx1000M -Xms100M -jar server.jar nogui
