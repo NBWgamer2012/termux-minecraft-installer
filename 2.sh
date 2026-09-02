d225451f266763f06e56fd701e88bea0aae9d629
NBWgamer2012 <nwaterman2012@gmail.com>
2026-09-02 16:15:07 -0700

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -2,6 +2,8 @@ apk update
 apk upgrade && apk add ca-certificates && update-ca-certificates && apk add nano && apk add openjdk17-jre
 java -version
 mkdir -p minecraft
+wget https://github.com/NBWgamer2012/termux-minecraft-installer/raw/refs/heads/main/startserver.sh
+chmod +x startserver.sh
 cd minecraft
 wget https://github.com/NBWgamer2012/termux-minecraft-installer/raw/refs/heads/main/minecraftserver.zip
 unzip minecraftserver.zip
